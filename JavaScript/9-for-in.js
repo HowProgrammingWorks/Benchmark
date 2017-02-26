1c1c2befb3f09cad811573c28cba9d6bac5608b4
tshemsedinov <timur.shemsedinov@gmail.com>
2017-02-26 02:20:21 +0200

    JavaScript for-in tests

---
+++ JavaScript/9-for-in.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const benchmark = require('./2-benchmark.js');
+
+const data = {
+  a: 'abc',
+  bcd: 'defg',
+  efgh: 'hijklmn',
+  ijk: 'opqrst',
+  lmnopqrs: 'u',
+  tvuwx: 'v',
+  yz: 'xyz'
+};
+
+function testForKeys() {
+  const a = Array(7);
+  let i, key;
+  const keys = Object.keys(data);
+  const len = keys.length;
+  for (i = 0; i < len; i++) {
+    key = keys[i];
+    a[i] = data[key];
+  }
+}
+
+
+function testForIn() {
+  const a = Array(7);
+  let i = 0;
+  for (let key in data) {
+    a[i++] = data[key];
+  }
+}
+
+function testForInLet() {
+  const a = Array(7);
+  let i = 0;
+  let key;
+  for (key in data) {
+    a[i++] = data[key];
+  }
+}
+
+function testForOf() {
+  const a = Array(7);
+  let i = 0;
+  let key, val;
+  const keys = Object.keys(data);
+  const len = keys.length;
+  for (key of keys) {
+    val = data[key];
+    a[i++] = val;
+  }
+}
+
+function testForOfLet() {
+  const a = Array(7);
+  let i = 0;
+  let key, val;
+  const keys = Object.keys(data);
+  const len = keys.length;
+  for (let key of keys) {
+    val = data[key];
+    a[i++] = val;
+  }
+}
+
+benchmark.do(1000000, 5, [
+  testForKeys,
+  testForIn,
+  testForInLet,
+  testForOf,
+  testForOfLet
+]);
