55d974198fd8d0a5399fbc90e27cdd92e797f893
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-03-07 13:43:42 +0200

    code style

--- JavaScript/e-inheritance.js
+++ JavaScript/e-inheritance.js
@@ -23,22 +23,7 @@ PrototypalDog.prototype.bark = function() {
 PrototypalDog.prototype.__proto__ = PrototypalAnimal.prototype;
 
 
-function FunctionalAnimal(name) {
-  this.name = name;
-  this.getName = function() {
-    return this.name;
-  };
-}
-
-function FunctionalDog(...arg) {
-  FunctionalAnimal.apply(this, arg);
-  this.bark = function() {
-    return `${this.name} is barking`;
-  };
-}
-
-
-class ES6Animal {
+class ClassBasedAnimal {
   constructor(name) {
     this.name = name;
   }
@@ -47,7 +32,7 @@ class ES6Animal {
   }
 }
 
-class ES6Dog extends ES6Animal {
+class ClassBasedDog extends ClassBasedAnimal {
   bark() {
     return `${this.name} is barking`;
   }
@@ -58,17 +43,12 @@ function createPrototypalDogs() {
   return dogNames.map((name) => new PrototypalDog(name));
 }
 
-function createFunctionalDogs() {
-  return dogNames.map((name) => new FunctionalDog(name));
-}
-
-function createES6Dogs() {
-  return dogNames.map((name) => new ES6Dog(name));
+function createClassBasedDogs() {
+  return dogNames.map((name) => new ClassBasedDog(name));
 }
 
 
 benchmark.do(100000, 3, [
   createPrototypalDogs,
-  createFunctionalDogs,
-  createES6Dogs
+  createClassBasedDogs
 ]);
