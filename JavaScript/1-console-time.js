35e8a7addc9a500374ecab28364e1831b688ceef
tshemsedinov <timur.shemsedinov@gmail.com>
2018-10-24 16:46:09 +0300

    Add simple example and improve output

--- JavaScript/1-console-time.js
+++ JavaScript/1-console-time.js
@@ -10,14 +10,26 @@ const fn = () => {
   return a;
 };
 
+console.log();
+
 console.time('experiment');
 const res1 = fn();
-console.log(res1.length);
+console.log('res1.length', res1.length);
 console.timeEnd('experiment');
 
-const begin = process.hrtime();
+console.log();
+
+const begin2 = new Date().getTime();
 const res2 = fn();
-console.log(res2.length);
-const end = process.hrtime(begin);
-const diff = end[0] * 1e9 + end[1];
-console.log(diff + ' nanoseconds');
+const end2 = new Date().getTime();
+const diff2 = end2 - begin2;
+console.dir({ length: res2.length, diff: diff2 });
+
+console.log();
+
+const begin3 = process.hrtime();
+const res3 = fn();
+const end3 = process.hrtime(begin3);
+const diff3 = end3[0] * 1e9 + end3[1];
+const sec3 = diff3 / 1e9;
+console.dir({ length: res3.length, msec: diff3, sec: sec3 });
